4b94fc4f457cefef172df2c382b6bb2c65c2da02
tamayoram <tamayoram@gmail.com>
2020-03-08 20:32:57 -0500

    Ejercicio para el almacen

---
+++ Almacen/Operaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Almacen
+{
+    class Operaciones
+    {
+        public int precio1 { get; set; }
+        public int precio2 { get; set; }
+        public int precio3 { get; set; }
+
+        public void operaciones(int precio1, int precio2, int precio3)
+        {
+            int valorTotal = 0;
+            int promedio = 0;
+
+            valorTotal = precio1 + precio2 + precio3;
+            promedio = valorTotal / 3;
+
+            Console.Write("El precio de los zapatos es " + precio1);
+            Console.Write("El precio de los pantalones es " + precio2);
+            Console.Write("El precio de los pantalones es " + precio3);
+
+            Console.Write("El valor total es " + valorTotal);
+
+            Console.Write("El precio promedio " + promedio);
+
+        }
+    }
+}
---
+++ Almacen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Almacen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el precio de los zapatos: ");
+            int zapatos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el precio de los pantalones: ");
+            int pantalones = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el precio de las camisetas: ");
+            int camisetas = int.Parse(Console.ReadLine());
+
+            var valoresAlmacen = new Operaciones()
+            {
+                precio1 = zapatos,
+                precio2 = pantalones,
+                precio3 = camisetas
+            };
+
+            valoresAlmacen.operaciones(valoresAlmacen.precio1, valoresAlmacen.precio2, valoresAlmacen.precio3);
+        }
+    }
+}
