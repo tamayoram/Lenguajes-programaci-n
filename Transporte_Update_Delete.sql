06d2b1dfdd29de2944853b5e77067b23165866b6
tamayoram <tamayoram@gmail.com>
2020-03-30 20:02:36 -0500

    Operaciones crear y consultar

--- Transporte_Update_Delete.sql
+++ Transporte_Update_Delete.sql
@@ -4,6 +4,8 @@ create database transporte
 
 use transporte
 
+/*Create*/
+
 create table conductor(
 Id  int primary key,
 nombre varchar(255),
@@ -23,6 +25,8 @@ alter table conductor
 add constraint FKvehiculo_conductor foreign key(id_vehiculo) references vehiculo(id_vehiculo)
 
 //procedimiento almacenado para insertar registros en conductor
+
+
 
 create proc registrarConductor
 (
@@ -48,12 +52,87 @@ create proc registrarVehiculo
 
 as insert into vehiculo values(@id_vehiculo,@marca,@modelo,@matricula)
 
-select * from conductor
 select * from vehiculo
+select * from conductor
 
 exec registrarVehiculo '1','Mazda','2018','DFS298'
 
 exec registrarConductor '1','Camilo','A','1','32'
 
 
+/*Create*/
 
+create table tipoConductor(
+Id  int primary key,
+nombre varchar(255),
+)
+
+create table tipoVehiculo(
+id_vehiculo int primary key,
+nombre varchar(255),
+)
+
+/*Procedimiento insert*/
+
+create proc registrarTipoConductor
+(
+	@Id  int,
+	@nombre varchar(255)
+)
+
+
+as insert into tipoConductor values(@Id,@nombre)
+
+create proc registrarTipoVehiculo
+(
+
+	@id_vehiculo int,
+	@nombre varchar(255)
+	
+)
+
+as insert into tipoVehiculo values(@id_vehiculo,@nombre)
+
+/*Procedimiento Update*/
+
+create proc actualizarTipoConductor
+(
+	@Id  int,
+	@nombre varchar(255)
+)
+
+as update tipoConductor set nombre=@nombre where Id=@Id
+
+
+create proc actualizarTipoVehiculo
+(
+
+	@id_vehiculo int,
+	@nombre varchar(255)
+	
+)
+
+as update tipoVehiculo set nombre=@nombre where id_vehiculo=@id_vehiculo
+
+
+/*Procedimiento Delete*/
+
+create proc eliminarTipoConductor
+(
+	@Id  int
+)
+
+as delete from tipoConductor where Id=@Id
+
+
+create proc eliminarTipoVehiculo
+(
+
+	@id_vehiculo int
+		
+)
+
+as delete from tipoVehiculo where id_vehiculo=@id_vehiculo
+
+select * from tipoVehiculo
+select * from tipoConductor
